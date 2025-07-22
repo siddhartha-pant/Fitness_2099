122e0426996d7b19b2d54460f6663a7820e2994e
siddhartha-pant <iamsiddhartha23@gmail.com>
2025-07-22 12:59:47 +0530

    Resumed

--- src/api/AuthAPI.jsx
+++ src/api/AuthAPI.jsx
@@ -1,6 +1,11 @@
 // src/api/authApi.js
-import API from './apiProvider';
+import API from "./apiProvider";
 
-export const signUpApi = (userData) => API.post('/auth/signup', userData);
-export const signInApi = (credentials) => API.post('/auth/signin', credentials);
-export const verifyEmailApi = (tokenData) => API.post('/auth/verify-email', tokenData); // Feature disabled on backend, but defining for completeness 
+export const signUpApi = (userData) => API.post("/auth/signup", userData);
+export const signInApi = (credentials) => API.post("/auth/signin", credentials);
+export const verifyEmailApi = (tokenData) =>
+  API.post("/auth/verify-email", tokenData); // Feature disabled on backend, but defining for completeness
+export const verifyUserProfile = (tokenData) =>
+  API.post("/api/v1/profile", tokenData);
+export const fetchUserData = (tokenData) =>
+  API.get("/api/v1/profile", tokenData);
